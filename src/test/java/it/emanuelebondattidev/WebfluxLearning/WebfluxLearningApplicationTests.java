89cb00b3ab669604da1b9282d3212cf9a747237b
Emanuele B <bondattiemanuele@gmail.com>
2025-07-27 11:43:21 +0200

    Here we go

---
+++ src/test/java/it/emanuelebondattidev/WebfluxLearning/WebfluxLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package it.emanuelebondattidev.WebfluxLearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
